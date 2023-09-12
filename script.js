be7458aa45da0a906db9c43c7dfb37fcbecbf434
Subhamshaw01 <137093444+Subhamshaw01@users.noreply.github.com>
2023-09-12 07:28:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-//your code here
+function strip(w)
+	{
+		let re=new RegExp('^(a|the|an)','i');
+		return w.replace(re,'').trim()
+	}
+const sortedBands=bands.sort((a,b)=>(strip(a)>strip(b))?1:-1)
 
